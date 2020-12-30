1dd0e783efe3c22f0299104716bbd4ce69f5d8dd
inhobyun <inho.byun@gmail.com>
2020-12-30 13:37:21 +0900

    asd

---
+++ asd_stuffs/asd_if_test.py
@@ -0,0 +1,59 @@
+"""
+client application for edge computing device
+coded functions as below
+- BLE sensor device; BOSCH SCD 110 scan & connect using bluepy.btle
+
+by Inho Byun, Researcher/KAIST
+   inho.byun@gmail.com
+                    started 2020-12-30
+"""
+import asyncio
+import datetime
+import socket
+import struct
+import sys
+import time
+from urllib import request, parse
+
+import ADS1256
+import RPi.GPIO as GPIO
+
+
+print ("ASD--> ADS1256 Test Program", flush=True)
+
+try:
+    ADC = ADS1256.ADS1256()
+    ADC.ADS1256_init()
+except :
+    GPIO.cleanup()
+    print ("ASD--> ADS1256 init fail, unknown error !", flush=True)
+    exit()
+
+cnt = 0
+t0 = t1 = time.time()
+while ( t1 - t0 < 0.5 ):
+        val_x = ADC.ADS1256_GetChannalValue(0)
+        t1 = time.time()
+        print ('ASD--> [%lf] at %f\x0d'%(val_x*5.0/0x7fffff, t1), flush=True)
+        print ("\33[2A", flush=True)
+        cnt += 1
+        
+print ('\nASD--> count [%d] time period: %f, %f'%(cnt, (t1-t0), (t1-t0)/cnt), flush=True)
+
+
+cnt = 0
+t0 = t1 = time.time()
+while ( t1 - t0 < 0.5 ):
+        val_x = ADC.ADS1256_GetChannalValue(0)
+        val_y = ADC.ADS1256_GetChannalValue(1)
+        val_z = ADC.ADS1256_GetChannalValue(2)
+        t1 = time.time()
+        print ('ASD--> [%lf][%lf][%lf] at %f\x0d'%(val_x*5.0/0x7fffff, val_y*5.0/0x7fffff, val_z*5.0/0x7fffff, t1), flush=True)
+        print ("\33[2A", flush=True)
+        cnt += 1
+        
+print ('\nASD--> count [%d] time period: %f, %f'%(cnt, (t1-t0), (t1-t0)/cnt), flush=True)
+
+GPIO.cleanup()
+
+print ("ASD--> completed", flush=True)
