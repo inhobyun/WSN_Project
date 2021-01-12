bfcce3c778276cac68651ead68142e5cc0dbe89f
inhobyun <inho.byun@gmail.com>
2021-01-12 12:38:11 +0900

    updated

--- wsn_server_SCD.py
+++ wsn_server_SCD.py
@@ -41,7 +41,7 @@ TCP_HOST_NAME   = socket.gethostname()
 TCP_PORT        = 8088              # Default TCP Port Name
 ##TCP_HTTP_PORT   = 5000            # origin flask WEB server port
 TCP_HTTP_PORT   = 8081              # Default WEB server port
-TCP_PACKET_MAX  = 10240             # max TCP packet size
+TCP_PACKET_MAX  = 4096              # max TCP packet size
 TCP_POLL_TIME   = 300.              # max time interval to poll TCP port
 TCP_ERR_CNT_MAX = 8                 # max unknown error count before reconnection
 #
